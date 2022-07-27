1e99e9ef3bd3c5d0a70d0ee21ed035069811a03e
Leon Vila <monnochromo@gmail.com>
2022-07-27 20:27:59 +0200

    Add scenery to the battle scene
    Medieval scenery added.
    
    Resolves: #9

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,171 @@
+using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+    public bool fightEnded;
+    public GameObject hitParticle;
+    private AudioManager audioManager;
+
+    public static Fighter player, enemy;
+
+    public TextMeshProUGUI clockText;
+    private int clockSeconds = 30;
+
+
+    public GameObject fightTextObject;
+
+    public TextMeshProUGUI playerName;
+    public Slider playerHealth;
+    public Slider playerStamina;
+
+    public TextMeshProUGUI enemyName;
+    public Slider enemyHealth;
+    public Slider enemyStamina;
+
+    void Awake()
+    {
+        audioManager = GetComponent<AudioManager>();
+    }
+
+    void Start()
+    {
+        playerName.text = player.name;
+        enemyName.text = enemy.name;
+
+        player.Freeze();
+        enemy.Freeze();
+
+        StartCoroutine(CountdowCoroutine());
+    }
+
+
+    void FixedUpdate()
+    {
+        HealthUpdate();
+        StaminaUpdate();
+    }
+
+    public void HealthUpdate()
+    {
+        playerHealth.value = player.health;
+        enemyHealth.value = enemy.health;
+    }
+
+    public void StaminaUpdate()
+    {
+        playerStamina.value = player.stamina;
+        enemyStamina.value = enemy.stamina;
+    }
+
+    public void AttackRegistered()
+    {
+    }
+
+    public void HitRegistered(Vector3 contactPoint)
+    {
+        Instantiate(hitParticle, contactPoint, hitParticle.transform.rotation);
+        audioManager.PlayRandomWithTag("hit");
+    }
+
+    public void BlockRegistered(Vector3 contactPoint)
+    {
+        audioManager.PlayRandomWithTag("block");
+    }
+
+    public void FightEnd(string tag)
+    {
+        fightEnded = true;
+
+        if (tag == "Player")
+        {
+            audioManager.Play("GameOver");
+            fightTextObject.GetComponent<TextMeshProUGUI>().text = "Game Over";
+            fightTextObject.GetComponent<CanvasGroup>().alpha = 1;
+            enemy.Celebration();
+        }
+        else
+        {
+            audioManager.Play("Victory");
+            fightTextObject.GetComponent<TextMeshProUGUI>().text = "Victory";
+            fightTextObject.GetComponent<CanvasGroup>().alpha = 1;
+            player.Celebration();
+        }
+
+        enemy.Freeze();
+        player.Freeze();
+    }
+
+    IEnumerator CountdowCoroutine()
+    {
+        audioManager.Play("Fight");
+
+        fightTextObject.GetComponent<TextMeshProUGUI>().text = "3";
+        yield return new WaitForSeconds(1);
+
+        fightTextObject.GetComponent<TextMeshProUGUI>().text = "2";
+        yield return new WaitForSeconds(1);
+
+        fightTextObject.GetComponent<TextMeshProUGUI>().text = "1";
+        yield return new WaitForSeconds(1);
+
+        fightTextObject.GetComponent<TextMeshProUGUI>().text = "Fight!";
+        fightTextObject.GetComponent<Animation>().Play("FadeOut");
+
+        player.Unfreeze();
+        enemy.Unfreeze();
+
+        bool endedByTime = false;
+
+        while (!fightEnded)
+        {
+            clockSeconds = clockSeconds - 1;
+            clockText.text = clockSeconds.ToString();
+
+            if (clockSeconds == 3)
+            {
+                audioManager.Play("Countdown");
+                fightTextObject.GetComponent<CanvasGroup>().alpha = 1;
+                fightTextObject.GetComponent<TextMeshProUGUI>().text = "3";
+            }
+
+            if (clockSeconds == 2)
+            {
+                fightTextObject.GetComponent<TextMeshProUGUI>().text = "2";
+            }
+
+            if (clockSeconds == 1)
+            {
+                fightTextObject.GetComponent<TextMeshProUGUI>().text = "1";
+            }
+
+            if (clockSeconds == 0)
+            {
+                fightTextObject.GetComponent<Animation>().Play("FadeOut");
+                fightTextObject.GetComponent<TextMeshProUGUI>().text = "Time's Up!";
+                enemy.Freeze();
+                player.Freeze();
+                endedByTime = true;
+                break;
+            }
+
+            yield return new WaitForSeconds(1);
+        }
+
+        if (endedByTime)
+        {
+            if (player.health > enemy.health)
+            {
+                FightEnd("Enemy");
+            }
+            else
+            {
+                FightEnd("Player");
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,5 +4,7 @@
 The 1following Unity Assets are used in the project:
 * [Fighting Motions Vol.1 by Magicpot Inc.](https://assetstore.unity.com/packages/3d/animations/fighting-motions-vol-1-76699)
 * [Hit Impact Effects by Travis Game Assets](https://assetstore.unity.com/packages/vfx/particles/hit-impact-effects-free-218385)
-* [Free Medieval 3D People Low Poly Models by Caftpix.net](https://craftpix.net/freebies/free-medieval-3d-people-low-poly-models/)
+* [Free Medieval 3D People Low Poly Models by Caftpix.net](https://craftpix.net/freebies/free-medieval-3d-people-low-poly-models)
+* [Free Medieval Houses 3D Low Poly Pack by Caftpix.net](https://craftpix.net/freebies/free-medieval-houses-3d-low-poly-pack)
+* [Free Tree 3D Low Poly Pack by Caftpix.net](https://craftpix.net/freebies/free-tree-3d-low-poly-pack)
 * [Free Fantasy Game GUI by pzUH](https://pzuh.itch.io/free-fantasy-game-gui)
